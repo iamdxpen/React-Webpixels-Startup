c449663f450772bd7392ff14eb3c9ba28ece32dc
pdevang <pdevang210@gmail.com>
2024-10-19 17:20:57 +0530

    webpixels add

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Home } from "./pages";
+import MainRouter from "./Router";
+
+function App() {
+  return (
+    <div>
+      <MainRouter></MainRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <>
+      <div>
+        <h1 class="text-3xl font-bold text-primary">Hello world!</h1>
+      </div>
+    </>
+  );
+}
+export default Home;
